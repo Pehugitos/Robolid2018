ef049123cd79bc8fc7d0709414bd7ab7347d085d
Canario0 <prenero4bc@gmail.com>
2018-03-30 19:32:33 +0200

    Primer Control Motor

--- src/app.ino
+++ src/app.ino
@@ -1,12 +1,24 @@
-void setup() {
+const int MOTORPIN = 3;
+
+
+void setup()
+{
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(MOTORPIN, OUTPUT);
+  digitalWrite(MOTORPIN, HIGH);
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+void loop()
+{
+  for(int speed =0; speed<=255; speed++){
+      analogWrite(MOTORPIN, speed);
+      delay(100);
+  }
+
+  // delay(100);
+  // analogWrite(3, 0);
+  // delay(10000);                       // wait for a second
+  // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  // delay(1000);                     // wait for a second
 }
