e27ce82b1b00c41b83fd1e41045bb94ad8d89a10
Canario0 <prenero4bc@gmail.com>
2018-04-09 00:59:02 +0200

    Añadido el código de los motores

--- src/app.ino
+++ src/app.ino
@@ -1,34 +1,26 @@
-const int MOTORPIN = 5;
-const int MOTORPIN2 = 3;
-int speed = 200;
+#include "Motor.h"
+const int MOTORS[] = {3, 5, 9, 10};
+const int NUM_MOTORS = 2;
+Motor *motorR = new Motor(0, 0, 3, 5);
+Motor *motorL = new Motor(0, 0, 9, 10);
 
 void setup()
 {
   // initialize digital pin LED_BUILTIN as an output.
-  pinMode(MOTORPIN, OUTPUT);
-  pinMode(MOTORPIN2, OUTPUT);
-  digitalWrite(MOTORPIN, HIGH);
-  digitalWrite(MOTORPIN2, HIGH);
+  for (int i = 0; i < NUM_MOTORS * 2; i++)
+  {
+    pinMode(MOTORS[i], OUTPUT);
+    digitalWrite(MOTORS[i], LOW);
+  }
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-  digitalWrite(MOTORPIN, LOW);
-  digitalWrite(MOTORPIN2, LOW);
-  for (int speed = 140; speed <= 255; speed += 10)
-  {
-    analogWrite(MOTORPIN, speed);
-
-    delay(1000);
-  }
-
-  digitalWrite(MOTORPIN, LOW);
-  digitalWrite(MOTORPIN2, LOW);
-  for (int speed = 140; speed <= 255; speed += 10)
-  {
-    analogWrite(MOTORPIN2, speed);
-
-    delay(1000);
-  }
+  motorR->setVelocidad(200);
+  motorL->setVelocidad(200);
+  delay(2000);
+  motorR->setVelocidad(0);
+  motorL->setVelocidad(0);
+  delay(3000);
 }
