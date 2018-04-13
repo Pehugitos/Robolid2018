0ab60ce904ad453d3c31413c7136e0cbe4f51f00
Canario0 <prenero4bc@gmail.com>
2018-04-13 16:50:24 +0200

    Añadido el proyecyo de android

---
+++ Android/app/src/main/java/com/pechuguitos/pipo/robolid/SerialActivity.java
@@ -0,0 +1,13 @@
+package com.pechuguitos.pipo.robolid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SerialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.serial);
+    }
+}
