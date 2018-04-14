39b798ffd1b8a505b223b8369ca160110afd6b8d
Canario0 <prenero4bc@gmail.com>
2018-04-14 13:01:16 +0200

    Descubriendo los dispositivos pareados

--- Android/app/src/main/java/com/pechuguitos/pipo/robolid/SerialActivity.java
+++ Android/app/src/main/java/com/pechuguitos/pipo/robolid/SerialActivity.java
@@ -1,13 +1,50 @@
 package com.pechuguitos.pipo.robolid;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class SerialActivity extends AppCompatActivity {
+    private BluetoothAdapter mBluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 2;
+    private EditText txt_Serial;
+    private Button btn_Send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.serial);
+        txt_Serial = findViewById(R.id.serial_in);
+        btn_Send = findViewById(R.id.serial_send);
+
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.d("Descubierto", deviceName + " " + deviceHardwareAddress);
+            }
+        }
     }
+
 }
