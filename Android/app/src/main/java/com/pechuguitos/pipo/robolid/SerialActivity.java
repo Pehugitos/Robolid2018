2f1a9bb6fec0975f79675b9c3dcf6f19b7f10630
Canario0 <prenero4bc@gmail.com>
2018-04-15 19:11:37 +0200

    Pestaña de búsqueda dispositivos creada

--- Android/app/src/main/java/com/pechuguitos/pipo/robolid/SerialActivity.java
+++ Android/app/src/main/java/com/pechuguitos/pipo/robolid/SerialActivity.java
@@ -2,15 +2,14 @@ package com.pechuguitos.pipo.robolid;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Set;
@@ -18,33 +17,41 @@ import java.util.Set;
 public class SerialActivity extends AppCompatActivity {
     private BluetoothAdapter mBluetoothAdapter;
     private int REQUEST_ENABLE_BT = 2;
-    private EditText txt_Serial;
-    private Button btn_Send;
+    private ListView lista;
+    private Button actualizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.serial);
-        txt_Serial = findViewById(R.id.serial_in);
-        btn_Send = findViewById(R.id.serial_send);
-
+        lista = findViewById(R.id.blue_list);
+        actualizar = findViewById(R.id.actualizar_btn);
+        actualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scan();
+            }
+        });
 
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!mBluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
+        } else
+            scan();
+    }
 
+    protected void scan() {
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
+        ArrayList<String> datos = new ArrayList<>();
         if (pairedDevices.size() > 0) {
             // There are paired devices. Get the name and address of each paired device.
             for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                Log.d("Descubierto", deviceName + " " + deviceHardwareAddress);
+                datos.add(device.getName() + "\n" + device.getAddress());
             }
+            Toast.makeText(this, "Seleccione un dispositivo.", Toast.LENGTH_SHORT).show();
         }
+        lista.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos));
     }
 
 }
---
+++ Android/app/src/main/java/com/pechuguitos/pipo/robolid/SliderActivity.java
@@ -0,0 +1,13 @@
+package com.pechuguitos.pipo.robolid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SliderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slider);
+    }
+}
